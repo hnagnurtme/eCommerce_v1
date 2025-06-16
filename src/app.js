0a67f1a04be1f23d2a896f336816e0d0d07956bd
Nguyễn Trung Ánh <anhnon0106@gmail.com>
2025-06-16 15:52:01 +0700

    [#1] RSA và JWT handle success

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ console.log('Process : ',process.env)
 app.use(morgan("dev"))
 app.use(helmet())
 app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended : true
+}))
 // init database
 require('./databases/init.mongodb')
 const { checkOverload} = require('./helper/check.connect')

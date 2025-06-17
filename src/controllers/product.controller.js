637970d3ae0e6f9a6e26654f83f119835d9af369
Nguyễn Trung Ánh <anhnon0106@gmail.com>
2025-06-17 16:35:15 +0700

    Factory pattern for api product

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,13 @@
+const ProductFactory = require("../services/product.service")
+const { OK, CREATED} = require('../core/sucess.response')
+class ProductController {
+    
+    createNewProduct = async (req, res, next) =>{
+        return new OK({
+            message : 'Create product successfully',
+            metadata : await ProductFactory.createProduct(req.body.product_type, req.body)
+        }).send(res)
+    }
+}
+
+module.exports = new ProductController()

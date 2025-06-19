6505b727a314bb9035c18b17db0eb15f331bcadb
Nguyễn Trung Ánh <anhnon0106@gmail.com>
2025-06-19 15:04:56 +0700

    fix refresh token

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -7,7 +7,8 @@ const { token } = require('morgan')
 const HEADER = {
     API_KEY: 'x-api-key',
     AUTHORIZATION: 'authorization',
-    CLIENT_ID: 'x-client-id'
+    CLIENT_ID: 'x-client-id',
+    REFRESH_TOKEN: 'x-rtoken-id'
 }
 
 const createTokenPair = async (payload, publicKey, privateKey) => {
@@ -91,7 +92,69 @@ const authenciation = asyncHandle(async (req, res, next) => {
     }
 })
 
-const verifyJWT = async ( token, keySecret) =>{
+
+
+const authenciationV2 = asyncHandle(async (req, res, next) => {
+    /*
+    1. Check user_id missing
+    2. Get access token
+    3. Verify token
+    4. Check user in db
+    5. Check key Store with userId
+    6. Return data
+    */
+
+    const userId = req.headers[HEADER.CLIENT_ID]
+
+    if (!userId) {
+        throw new NotFoundError(' Khong co client id')
+    }
+
+    const keyStore = await findByUserId(userId)
+
+    if (!keyStore) {
+        throw new NotFoundError(' Khong co key Store')
+    }
+
+    // Check refreshToken
+    if (req.headers[HEADER.REFRESH_TOKEN]) {
+        try {
+            const refreshToken = req.headers[HEADER.REFRESH_TOKEN]
+            const decodedUser = jwt.verify(refreshToken, keyStore.privateKey)
+            if (userId != decodedUser.userId) {
+                throw new NotFoundError(' Ko cung id')
+            }
+
+            req.keyStore = keyStore
+            req.user = decodedUser
+            req.refreshToken = refreshToken
+            return next()
+        } catch (error) {
+            throw error
+        }
+    }
+
+    const accessToken = req.headers[HEADER.AUTHORIZATION]
+
+    if (!accessToken) {
+        throw new NotFoundError('Khong co access token')
+    }
+
+    try {
+        const decodedUser = jwt.verify(accessToken, keyStore.privateKey)
+        if (userId != decodedUser.userId) {
+            throw new NotFoundError(' Ko cung id')
+        }
+
+        req.keyStore = keyStore
+        req.user = decodedUser
+        return next()
+    } catch (error) {
+        throw error
+    }
+})
+
+const verifyJWT = async (token, keySecret) => {
     return await jwt.verify(token, keySecret)
 }
 
@@ -99,5 +162,6 @@ const verifyJWT = async ( token, keySecret) =>{
 module.exports = {
     createTokenPair,
     authenciation,
+    authenciationV2,
     verifyJWT
 }
--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const accessController = require('../../controllers/access.controller');
 const  asyncHandle  = require('../../helper/asyncHandle');
-const { authenciation } = require('../../auth/authUtils');
+const { authenciation , authenciationV2} = require('../../auth/authUtils');
 const route = express.Router()
 
 
@@ -9,7 +9,7 @@ route.post('/shop/signup', asyncHandle(accessController.signUp))
 route.post('/shop/login', asyncHandle(accessController.logIn))
 
 
-route.use(authenciation)
+route.use(authenciationV2)
 route.post('/shop/refreshToken', asyncHandle(accessController.handleRefreshToken))
 route.post('/shop/logout', asyncHandle(accessController.logOut))
 module.exports = route
